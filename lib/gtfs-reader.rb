8e5aead2919499e33d09476d9e56fbad20432535
Jon Sangster <jon@ertt.ca>
2014-02-26 02:19:58 -0400

    Add version and bump rake task

---
+++ lib/gtfs-reader.rb
@@ -0,0 +1,3 @@
+module GtfsReader
+  VERSION = '0.1.0'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+require 'simplecov'
+SimpleCov.start
+
+RSpec.configure do |config|
+  # config.filter_run :focus
+  # config.run_all_when_everything_filtered = true
+
+  if config.files_to_run.one?
+    config.full_backtrace = true
+    config.formatter = 'doc' if config.formatters.none?
+  end
+
+  config.profile_examples = 10
+  config.order = :random
+
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+end
