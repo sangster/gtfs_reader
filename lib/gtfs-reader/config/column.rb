ed1bcf39323e89162b391fba1626d076ea132bda
Jon Sangster <jsangster@uniqueltd.com>
2014-02-27 18:42:59 -0400

    document column.rb

--- lib/gtfs-reader/config/column.rb
+++ lib/gtfs-reader/config/column.rb
@@ -1,12 +1,22 @@
-module GtfsReader::Config  
+module GtfsReader::Config
+  # Defines a single column in a {FileFormat file}.
   class Column
-    NO_OP_PARSER = ->(arg) { arg }
+    # A "parser" which simply returns its input. Used by default
+    IDENTITY_PARSER = ->(arg) { arg }
 
     attr_reader :name, :parser
 
+    #@param name [String] the name of the column
+    #@option opts [Boolean] :required (false) If this column is required to
+    #  appear in the given file
+    #@option opts [String] :alias an alternative name for this column. Many
+    #  column names are needlessly prefixed with their filename:
+    #  +Stop.stop_name+ could be aliased to +Stop.name+ for example.
+    #@option opts [Boolean] :unique (false) if values in this column need to be
+    #  unique among all rows in the file.
     def initialize(name, opts={}, &parser)
       @name = name
-      @parser = block_given? ? parser : NO_OP_PARSER
+      @parser = block_given? ? parser : IDENTITY_PARSER
 
       @opts = { 
         required: false, 
@@ -15,14 +25,18 @@ module GtfsReader::Config
       }.merge (opts || {})
     end
 
+    #@return [Boolean] if this column is required to appear in the file
     def required?
       @opts[:required]
     end
 
+    #@return [Boolean] if values in this column need to be unique among all rows
+    #  in the file.
     def unique?
       @opts[:unique]
     end
 
+    #@return [String,nil] this column's name's alias, if there is one
     def alias
       @opts[:alias]
     end
