ed3ea3703aa4b79af8eab600e8b8d3449b80d939
Jon Sangster <jon@ertt.ca>
2014-04-10 00:56:23 -0300

    Fix missing Log4j:: error

--- lib/gtfs_reader/version.rb
+++ lib/gtfs_reader/version.rb
@@ -5,7 +5,7 @@ module GtfsReader
     # The following four lines are generated, so don't mess with them.
     MAJOR = 0
     MINOR = 2
-    PATCH = 3
+    PATCH = 4
     BUILD = nil
 
     #@return [String] the current version in the form of +1.2.3.build+
