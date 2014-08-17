7c55576fcf213e29a20cb1259a7c63bbde367f5b
Jon Sangster <jon@ertt.ca>
2014-08-17 19:53:18 -0300

    Warn that the gem is being renamed

--- lib/gtfs_reader/version.rb
+++ lib/gtfs_reader/version.rb
@@ -5,7 +5,7 @@ module GtfsReader
     # The following four lines are generated, so don't mess with them.
     MAJOR = 0
     MINOR = 2
-    PATCH = 8
+    PATCH = 10
     BUILD = nil
 
     #@return [String] the current version in the form of +1.2.3.build+
